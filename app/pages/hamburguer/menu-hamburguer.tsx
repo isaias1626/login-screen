04236015b545aaec5fdac4f5eb806479743925a7
¨isaias <isaiasrocha1626@gmail.com>
2024-06-14 04:19:27 -0300

    feat: add resposive and sidebar

--- app/pages/hamburguer/menu-hamburguer.tsx
+++ app/pages/hamburguer/menu-hamburguer.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { ChevronLeft, MenuIcon, Home, Box, Grid, School, Clipboard, User } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { ChevronLeft, MenuIcon,} from "lucide-react";
 import { useState } from "react";
-import LogoMenu from "@/public/logoMenu.png"
+import LinksSidebar from "../sidebar/link-sidebar";
 
 const MenuHamburguer = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -24,19 +22,11 @@ const MenuHamburguer = () => {
             {isOpen && (
                 <div>
                     <div onClick={toggleMenu}  className="absolute inset-0 z-10 bg-black bg-opacity-50"></div>
-                    <div className="absolute top-0 z-10 w-[248px] py-2 left-0 bg-white shadow-lg h-screen px-6">
-                        <div className="mb-6 mt-6">
-                            <Image className="w-[70px]" src={LogoMenu} alt="logo do menu hamburguer" />
-                        </div>
-                            <Link href="/" className="mb-2 text-md font-semibold hover:text-orange-500 flex px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-full"><Home className="w-5 mr-4" /> Home</Link>
-                            <Link href="#" className="mb-2 text-md font-semibold hover:text-orange-500 flex px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-full"><Box className="w-5 mr-4" /> Estoque</Link>
-                            <Link href="#" className="mb-2 text-md font-semibold hover:text-orange-500 flex px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-full"><Grid className="w-5 mr-4" /> Categorias</Link>
-                            <Link href="#" className="mb-2 text-md font-semibold hover:text-orange-500 flex px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-full"><School className="w-5 mr-4" /> Escolas</Link>
-                            <Link href="#" className="mb-2 text-md font-semibold hover:text-orange-500 flex px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-full"><Clipboard className="w-5 mr-4" /> Gestão de pedidos</Link>
-                            <Link href="#" className="mb-2 text-md font-semibold hover:text-orange-500 flex px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-full"><User className="w-5 mr-4" /> Úsuarios</Link>
-                            <div onClick={toggleMenu} className="mb-2 text-md font-semibold hover:text-orange-500 flex px-2 py-2 text-gray-800 hover:bg-gray-200 rounded-full cursor-pointer"><ChevronLeft className="w-5 mr-4" /> Retornar</div>
+                    <div className="absolute top-0 z-10 w-[248px] lg:w-[300px] py-2 left-0 bg-white shadow-lg h-screen px-6">
                         <div>
-                    </div>
+                            <LinksSidebar />
+                            <div onClick={toggleMenu} className="mb-2 text-md font-semibold hover:text-orange-500 flex px-2 py-2 text-gray-800 hover:bg-gray-200 rounded-full cursor-pointer"><ChevronLeft className="w-5 mr-4" /> Retornar</div>
+                        </div>
                     </div>
                 </div>
             )}
---
+++ app/pages/sidebar/sidebar-destop.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useState } from "react";
+import LinksSidebar from "./link-sidebar";
+import { ChevronLeft } from "lucide-react";
+
+const Sidebar = () => {
+    const [width, setWidth] = useState("70px");
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const toggleWidth = () => {
+        if (width === "70px") {
+            setWidth("300px");
+            setIsCollapsed(false);
+        } else {
+            setWidth("70px");
+            setIsCollapsed(true);
+        }
+    };
+
+    return (
+        <div>
+            <nav className="bg-white top-0 h-full overflow-y-auto" style={{ width }}>
+                <div>
+                    <div>
+                        <div>
+                            <LinksSidebar />
+                        </div>
+                        <div className="flex ml-5 pt-4">
+                            <button 
+                                className="flex items-center text-md font-semibold transition-transform duration-300" 
+                                onClick={toggleWidth} 
+                                type="button"
+                            >
+                                <ChevronLeft 
+                                    className={`mr-5 w-8 transition-transform duration-300 ${isCollapsed ? 'rotate-180' : ''}`} 
+                                />
+                                Recolher
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Sidebar;
